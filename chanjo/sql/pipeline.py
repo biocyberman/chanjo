f8a8448193183b563eb49e9a4993d60dfa4d017f
Robin Andeer <robin.andeer@gmail.com>
2014-03-13 15:53:59 +0100

    Correctly dealing with bytes/unicode crap

--- chanjo/sql/pipeline.py
+++ chanjo/sql/pipeline.py
@@ -10,17 +10,23 @@ from .utils import read_supersets, assemble_superset
 # | Import pipeline
 # +--------------------------------------------------------------------+
 def import_from_ccds(db, ccds_path):
+  # Set up column names and types (otherwise we get bytes in Python 3)
+  columns = [('chromosome', '<U12'), ('nc_accession', '<U12'),
+             ('gene', '<U24'), ('gene_id', int), ('ccds_id', '<U12'),
+             ('ccds_status', '<U12'), ('cds_strand', '<U12'),
+             ('cds_from', int), ('cds_to', int), ('cds_locations', '<U7700'),
+             ('match_type', '<U12')]
   # Dump the file into memory
-  all_sets = np.recfromcsv(ccds_path, delimiter='\t')
+  all_sets = np.genfromtxt(ccds_path, delimiter='\t', dtype=columns)
 
   # Filter out all non-Y records
-  non_y = all_sets[all_sets['chromosome'] != b'Y']
-  all_y = all_sets[all_sets['chromosome'] == b'Y']
+  non_y = all_sets[all_sets['chromosome'] != 'Y']
+  all_y = all_sets[all_sets['chromosome'] == 'Y']
 
   for full_set in [non_y, all_y]:
 
     # Filter by status code
-    public_sets = full_set[full_set['ccds_status'] == b'Public']
+    public_sets = full_set[full_set['ccds_status'] == 'Public']
 
     # Sort by HGNC symbol
     sorted_sets = public_sets[public_sets['gene'].argsort()]
